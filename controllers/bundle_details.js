ba524248e7e7a5fba9b74cd96d6a1eab28adb494
Jeswin <jeswin.zcodia@gmail.com>
2018-07-30 19:28:16 +0530

    Existing wishlist display in product and bundle list and details pages completed

--- controllers/bundle_details.js
+++ controllers/bundle_details.js
@@ -2,9 +2,15 @@ const axios_config = require("../config/axios-config.js").instance;
 const axios = require("axios");
 // const Logger = require("../lib/logger").Logger;
 
-let get_data = async pb_id => {
+let get_data = async (pb_id, cookies) => {
   try {
     axios_config['params']= {product_id: pb_id};
+    if(cookies){
+      var userData= JSON.parse(cookies);
+      axios_config["headers"] = {
+        Authorization: "Bearer " + userData.token
+      };
+    }
     let response = await axios.get(`/product/details_pb/get`, axios_config);
     return response.data.results;
   } catch (error) {
