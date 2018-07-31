7daad838b80e482c59189b5f0c71499abaf66654
Jeswin <jeswin.zcodia@gmail.com>
2018-07-31 17:25:46 +0530

    UI integeration in Product and Bundle Details Pages Completed

--- routes/checkout.js
+++ routes/checkout.js
@@ -4,5 +4,28 @@ var router = express.Router();
 /* GET cart page. */
 router.get('/', function(req, res, next) {
   res.render('payment', { title: 'Express' });
+  try {
+    let cookies = !req.cookies.vcartAuth ? false : req.cookies.vcartAuth;
+    if (cookies == false) {
+      res.redirect("/");
+      res.end();
+    } else {
+      res.render("payment", {
+        cookies: cookies,
+        angular: true,
+        customjs: true,
+        jslist: [
+          "angular/app.js",
+          "angular/factory/cart.js",
+          "angular/controllers/cart.js"
+        ]
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      error: error
+    });
+  }
 });
 module.exports = router;
