92a29e17b51a9c81237ac3cb3237793951de8652
Siva Murugan <shivazcodia@gmail.com>
2018-08-03 17:20:02 +0530

    Order list completed

--- routes/myorders.js
+++ routes/myorders.js
@@ -1,18 +1,27 @@
 var express = require("express");
 var router = express.Router();
 const getMenu = require("../controllers/category_menu.js");
+const myOrders = require("../controllers/myorders.js");
 
 /* GET product list page. */
 router.get("/", async (req, res, next) => {
   try {
     let cookies = !req.cookies.vcartAuth ? false : req.cookies.vcartAuth;
     let menudata = await getMenu.get_menulist();
-    res.render("myorders", {
-      menudata: menudata,
-      angular: false,
-      customjs: false,
-      cookies: cookies
-    });
+    
+    if (cookies == false) {
+      res.redirect("/");
+      res.end();
+    } else {
+      let data = await myOrders.get_data(JSON.parse(cookies));
+      res.render("myorders", {
+        menudata: menudata,
+        data:data,
+        angular: false,
+        customjs: false,
+        cookies: cookies
+      });
+    }
   } catch (error) {
     console.log(error);
     res.status(401).json({
