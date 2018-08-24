ef0b8304b28eb29d302dc909eb1f1bb5f8b2b468
ganesh <ganesh@192.168.1.4>
2018-08-24 19:58:54 +0530

    Breadcrumb Corrections (Working In Progress)

--- routes/product-detail.js
+++ routes/product-detail.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const productDetail = require("../controllers/product_detail.js");
+const getMenu = require("../controllers/category_menu.js");
 
 /* Get product details */
 router.get("/:pd_id/:pd_name", async (req, res, next) => {
@@ -9,8 +10,10 @@ router.get("/:pd_id/:pd_name", async (req, res, next) => {
     let pd_id = req.param("pd_id");
     // let pd_name = req.param("pd_name");
     let data = await productDetail.get_data(pd_id, cookies);
+    let menudata = await getMenu.get_menulist();
     res.render("productdetail", {
       data: data,
+      menudata: menudata,
       cookies: cookies,
       angular: true,
       customjs: true,
