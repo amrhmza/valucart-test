64ac87eee16befa7c0648c5e9a86288df89170b1
karthick <karthick@kutung.com>
2018-10-29 18:54:29 +0530

    Localhost url removed

--- public/js/search.js
+++ public/js/search.js
@@ -1,15 +1,15 @@
 var getUrlParameter = function getUrlParameter(param, dummyPath) {
   var sPageURL = dummyPath || window.location.search.substring(1),
-      sURLVariables = sPageURL.split(/[&||?]/),
-      res;
+    sURLVariables = sPageURL.split(/[&||?]/),
+    res;
 
   for (var i = 0; i < sURLVariables.length; i += 1) {
-      var paramName = sURLVariables[i],
-          sParameterName = (paramName || '').split('=');
+    var paramName = sURLVariables[i],
+      sParameterName = (paramName || '').split('=');
 
-      if (sParameterName[0] === param) {
-          res = sParameterName[1];
-      }
+    if (sParameterName[0] === param) {
+      res = sParameterName[1];
+    }
   }
 
   return res;
@@ -28,11 +28,9 @@ $(".desktopsearch").keydown(function (event) {
   }
 });
 
-if(getUrlParameter('q')){
+if (getUrlParameter('q')) {
   $(".desktopsearch").val(decodeURIComponent(getUrlParameter('q')));
 }
-
-APIURL = 'https://localhost:3000';
 
 $(".desktopsearch")
   .autocomplete({
@@ -65,52 +63,52 @@ $(".desktopsearch")
     select: function (event, ui) {
       var base_url = window.location.origin;
       d = ui.item;
-        switch (d.desc) {
-          case "product":
-            window.location.replace(
-              base_url +
-              "/product-detail/" +
-              d.id +
-              "/" +
-              d.value.replace(/\s+/g, "-")
-            );
-            break;
-          case "brand":
-            window.location.replace(
-              base_url + "/product-listing/" + "#!?brand=" + d.id
-            );
-            break;
-          case "bundle":
-            window.location.replace(
-              base_url +
-              "/bundle-detail/" +
-              d.id +
-              "/" +
-              d.value.replace(/\s+/g, "-")
-            );
-            break;
-          case "category":
-            window.location.replace(
-              base_url +
-              "/product-listing/" +
-              d.id +
-              "/" +
-              d.value.replace(/\s+/g, "-")
-            );
-            break;
-          case "sub-category":
-            window.location.replace(
-              base_url +
-              "/product-listing/" +
-              d.main_id +
-              "/" +
-              d.value.replace(/\s+/g, "-") +
-              "#!?sub_cat=" +
-              d.id
-            );
-            break;
-        }
+      switch (d.desc) {
+        case "product":
+          window.location.replace(
+            base_url +
+            "/product-detail/" +
+            d.id +
+            "/" +
+            d.value.replace(/\s+/g, "-")
+          );
+          break;
+        case "brand":
+          window.location.replace(
+            base_url + "/product-listing/" + "#!?brand=" + d.id
+          );
+          break;
+        case "bundle":
+          window.location.replace(
+            base_url +
+            "/bundle-detail/" +
+            d.id +
+            "/" +
+            d.value.replace(/\s+/g, "-")
+          );
+          break;
+        case "category":
+          window.location.replace(
+            base_url +
+            "/product-listing/" +
+            d.id +
+            "/" +
+            d.value.replace(/\s+/g, "-")
+          );
+          break;
+        case "sub-category":
+          window.location.replace(
+            base_url +
+            "/product-listing/" +
+            d.main_id +
+            "/" +
+            d.value.replace(/\s+/g, "-") +
+            "#!?sub_cat=" +
+            d.id
+          );
+          break;
       }
+    }
   })
   .autocomplete("instance")._renderItem = function (ul, item) {
     return $("<li>")
